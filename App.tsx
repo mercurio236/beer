8b11b3a35c8e42b88c4ef6cbf019669b784f84e8
Arley Souto <macx15@hotmail.com>
2023-09-26 02:55:01 -0300

    chore: configuração png e styled-components

--- App.tsx
+++ App.tsx
@@ -1,22 +1,32 @@
 import { StatusBar } from 'react-native'
 import { Routes } from './src/routes'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components/native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import theme from './src/theme'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { store } from './src/libs/redux/store'
 import { Provider } from 'react-redux'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Provider store={store}>
-        <Routes />
-      </Provider>
+      <SafeAreaProvider
+        style={{ flex: 1, backgroundColor: theme.COLORS.GRAY_100 }}
+      >
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <Provider store={store}>
+          <Routes />
+        </Provider>
+      </SafeAreaProvider>
     </ThemeProvider>
   )
 }
--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,8 +1,9 @@
 import 'styled-components'
 import theme from '../theme'
 
-declare module 'styled-componets' {
-  type ThemeType = typeof theme
+declare module 'styled-components'{
+    type ThemeType = typeof theme;
 
-  export interface DefaultTheme extends ThemeType {}
+
+    export interface DefaultTheme extends ThemeType { }
 }
