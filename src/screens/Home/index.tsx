31c1c99a1ce013484e49677fb508392e599809a6
Arley Souto <macx15@hotmail.com>
2023-09-26 20:55:51 -0300

    feat: adicionado loading na home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
-import { View, Text, FlatList } from 'react-native'
+import { Alert, FlatList } from 'react-native'
 
 import { api } from '../../libs/services/axios'
 import { ContainerHome } from './styles'
 import { Header } from '@components/Header'
 import { BeerList } from '@components/BeerList'
+import { ListEmpty } from '@components/ListEmpty'
+import { Loading } from '@components/Loading'
 
 export interface BeersProps {
   id: number
@@ -15,17 +17,27 @@ export interface BeersProps {
 
 export function Home() {
   const [beers, setBeers] = useState<BeersProps[]>([])
+  const [isLoading, setIsLoading] = useState(true)
 
   async function fetchListProducts() {
     try {
+      setIsLoading(true)
+
       const response = await api.get('beers', { params: { per_page: 8 } })
+
       setBeers(response.data)
     } catch (error) {
       console.log(error)
+      Alert.alert(
+        'Erro ao carregar as bebidas',
+        'Não foi possível carregar as bebidas. Tente novamente mais tarde.'
+      )
+    } finally {
+      setIsLoading(false)
     }
   }
 
-  function handleDatilBeer(id: number){
+  function handleDatilBeer(id: number) {
     console.log(id)
   }
 
@@ -36,12 +48,21 @@ export function Home() {
   return (
     <ContainerHome>
       <Header title="Bebidas" />
-      <FlatList
-        data={beers}
-        keyExtractor={(item) => String(item.id)}
-        renderItem={({ item }) => <BeerList data={item} onPress={() => handleDatilBeer(item.id)}/>}
-        showsVerticalScrollIndicator={false}
-      />
+      {isLoading ? (
+        <Loading  />
+      ) : (
+        <FlatList
+          data={beers}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => (
+            <BeerList data={item} onPress={() => handleDatilBeer(item.id)} />
+          )}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <ListEmpty message="Lista de bebidas vazia." />
+          )}
+        />
+      )}
     </ContainerHome>
   )
 }
