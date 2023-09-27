fbeb90baf88dc47a2ca901601e73925d147fd38f
Arley Souto <macx15@hotmail.com>
2023-09-27 01:18:09 -0300

    feat: iniciado a construção da página de detalhes

--- src/screens/Products/index.tsx
+++ src/screens/Products/index.tsx
@@ -1,10 +1,20 @@
-import {View, Text} from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { View, Text, Button } from 'react-native'
+import { useSelector } from 'react-redux'
+import { RootState } from 'src/libs/redux/store'
 
-
-export function Products(){
-    return(
-        <View>
-            <Text>Products</Text>
-        </View>
-    )
+export function Products() {
+    const navigation = useNavigation()
+  const { beer } = useSelector((state: RootState) => state.products)
+  console.log('Page Products => ', beer)
+  
+  function handleGoBack(){
+    navigation.goBack()
+  }
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>{JSON.stringify(beer)}</Text>
+      <Button title='Voltar' onPress={handleGoBack}/>
+    </View>
+  )
 }
